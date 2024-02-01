90cb072a9a3c6f6830163808640eb476d42310c8
ISB-VIETNAM\khanhnguyen.446 <khanhnguyen.446@vn.isb.co.jp>
2024-02-01 09:41:33 +0700

    chore: watch hook

--- pages/vueusecore/index.vue
+++ pages/vueusecore/index.vue
@@ -1,7 +1,12 @@
 <script setup>
 import { OnClickOutside } from "@vueuse/components";
 import { useGlobalState } from "@/stores/globalState";
-import { useDebouncedRefHistory } from "@vueuse/core";
+import {
+  useDebouncedRefHistory,
+  useLastChanged,
+  useStorage,
+  useActiveElement,
+} from "@vueuse/core";
 
 const { $api } = useNuxtApp();
 
@@ -13,6 +18,8 @@ const store = useLocalStorage("my-storage", {
   name: "Apple",
   color: "red",
 });
+// bind object
+const state = useStorage("my-store", { hello: "hi", greeting: "Hello" });
 
 useEventListener("mousemove", () => {
   console.log("cleanup mousemove");
@@ -31,18 +38,39 @@ const reset = () => {
 };
 
 const { data: videos } = await $api.videos.getAllVideos();
-const sddata = useCustomVueCoreFetch();
-console.log("fetched", sddata, videos);
+const sdata = useCustomVueCoreFetch();
+console.log("fetched", sdata);
 
 const employees = ref({
-  firstname: '',
-  lastname: '',
-  email: ''
-})
-const { undo: undoForm, redo: redoForm } = useDebouncedRefHistory(employees,{
+  firstname: "",
+  lastname: "",
+  email: "",
+});
+const { undo: undoForm, redo: redoForm } = useDebouncedRefHistory(employees, {
   deep: true,
-  debounce: 1000
-})
+  debounce: 1000,
+});
+
+const txtLastChange = ref("");
+const lastChange = useLastChanged(txtLastChange, { flush: "sync" });
+console.log("lastchange", lastChange);
+
+onMounted(() => {
+  console.log("mounted hook");
+  // localStorage.setItem('my-store', '{"hello": "hello"}')
+  console.log("useLocalStorage", localStorage.getItem("my-storage"));
+  console.log("useStorage", localStorage.getItem("my-store"));
+});
+
+const activeElement = useActiveElement();
+
+watch(
+  txtLastChange,
+  (to, from) => {
+    console.log("tracking everything is changed here", '|| old value: ', from, '|| new value: ', to);
+  },
+  { deep: true, immediate: true }
+);
 </script>
 
 <template>
@@ -100,6 +128,13 @@ const { undo: undoForm, redo: redoForm } = useDebouncedRefHistory(employees,{
       <v-btn class="me-1" variant="tonal" @click="undoForm">UndoForm</v-btn>
       <v-btn variant="tonal" @click="redoForm">RedoForm</v-btn>
 
+      <hr />
+      <h3>useLastChanged</h3>
+      <v-text-field
+        v-model="txtLastChange"
+        label="Type something"
+      ></v-text-field>
+      <p>{{ lastChange === null ? "" : new Date(lastChange) }}</p>
     </ClientOnly>
   </div>
 </template>
